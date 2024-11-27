2443ab5a15292b7de509383c627cbf84fa03cbb1
aman tyagi <amantyagi7024@gmail.com>
2024-11-27 12:00:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+document.getElementById("demo").innerHTML="hello world!";
+// dusra way
+var h1=document.getElementById("demo");
+h1.innerHTML="hello world";
+// h1 ke sthan par heading bhi likh sakte hai 
+// changing the css of element 
+h1.style.color="red";
+h1.style.backgroundColor ="yellow";
+h1.style.display="flex";
+h1.style.justifyContent="center";
+h1.style.alignItems="center";
+
+
+// padding , border, fontsize, height apply
+h1.style.padding="20px 50px 80px 100px";
+h1.style.border="10px solid red";
+h1.style.fontSize="60px";
+h1.style.height="120px";
+var buton=document.getElementById("btn");
+buton.innerHTML="show";
+buton.style.color="green";
+buton.style.backgroundColor="red";
+buton.style.fontSize="50px";
+buton.style.borderRadius="4px";
+buton.style.display="flex";
+buton.style.justifyContent="center";
+buton.style.alignItems="center";
+buton.style.boxShadow="20px -1px";
+{/* <home work> */}
+var h2=document.getElementById("new");
+h2.innerHTML="I am learning java script";
+h2.style.backgroundColor="red";
+h2.style.color="white";
+h2.style.textAlign="center";
+h2.style.fontFamily='cursive';
+h2.style.marginTop='50px';
+
+var p=document.getElementById("new1");
+p.innerHTML="java script is a programming language variable means for data store we use variables  javascript clint side and server side language hai java script is a programming language variable means for data store we use variables  javascript clint side and server side language java script is a programming language variable means for data store we use variables  javascript clint side and server side language hai java script is a programming language variable means for data store we use variables  javascript clint side and server side language hai java script is a programming language variable means for data store we use variables  javascript clint side and server side language java script is a programming language variable means for data store we use variables  javascript clint side and server side language hai java script is a programming language variable means for data store we use variables  javascript clint side and server side language hai java script is a programming language variable means for data store we use variables  javascript clint side and server side language java script is a programming language variable means for data store we use variables  javascript clint side and server side language haijava script is a programming language variable means for data store we use variables  javascript clint side and server side language hai java script is a programming language variable means for data store we use variables  javascript clint side and server side language java script is a programming language variable means for data store we use variables  javascript clint side and server side language hai ";
+p.style.backgroundColor="black";
+p.style.color="white";
+p.style.fontSize="20px";
+p.style.display="flex";
+p.style.justifyContent="center";
+p.style.alignItems='center';
+p.style.textAlign="center"
+var a=document.getElementById("login");
+a.innerHTML=("Login");
+a.style.textDecoration="none";
+a.style.borderRadius="39px;"
+a.style.display='flex';
+a.style.justifyContent="center";
+a.style.fontSize="30px";
+a.style.backgroundColor="red";
+// a.style.textAlign="center";
+
+var aman=document.getElementById("aaaa");
+aman.innerHTML
+aman.style.color="red";
+aman.style.backgroundColor="green";
+aman.style.textAlign="center";
+aman.style.padding="10px";
+aman.style.margin="50px";
